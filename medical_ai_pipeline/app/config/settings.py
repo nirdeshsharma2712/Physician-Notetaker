cf76bccc546f161451dd0f0adea13741e4c4f238
nirdeshsharma2712 <138863168+nirdeshsharma2712@users.noreply.github.com>
2025-12-22 20:58:08 +0530

    Update settings.py

--- medical_ai_pipeline/app/config/settings.py
+++ medical_ai_pipeline/app/config/settings.py
@@ -1,1 +1,34 @@
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+DATA_DIR = BASE_DIR / "data"
+RAW_TRANSCRIPTS_DIR = DATA_DIR / "raw_transcripts"
+
+MEDICAL_NER_MODEL = "en_ner_bc5cdr_md"
+SENTIMENT_MODEL = "distilbert-base-uncased"
+INTENT_MODEL = "distilbert-base-uncased"
+
+MAX_SEQUENCE_LENGTH = 512
+DEVICE = "cpu"  
+
+ALLOW_INFERENCE = False   
+
+DOCTOR_ALIASES = {
+    "doctor",
+    "physician",
+    "dr",
+    "Physician"
+    "dr.",
+    "surgeon",
+    "provider",
+    "clinician"
+}
+
+PATIENT_ALIASES = {
+    "patient",
+    "pt",
+    "patiient",
+    "patientdocter" 
+}
